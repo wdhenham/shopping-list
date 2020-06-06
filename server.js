b9869483457e85af5d2c836e9d8eeb7c0edacd16
wdhenham <wdhenham@gmail.com>
2020-06-06 14:47:21 +1000

    updating port

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use(express.static('./public'));
 listController(app, bodyParser);
 
 //listen to port
-app.listen(3000);
-console.log('You are listening to port 3000');
+app.listen(8080);
+console.log('You are listening to port 8080');
